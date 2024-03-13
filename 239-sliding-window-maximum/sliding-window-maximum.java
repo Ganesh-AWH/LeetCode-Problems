c1fdbe9ba520467a860d32c41c51e6ac0323623d
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-03-13 21:32:41 +0530

    Time: 32 ms (53.74%) | Memory: 59.5 MB (39.02%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        //solve the problem using double ended queue since we have remove or add element from front and last
+        Deque<Integer> deq = new ArrayDeque<>();
+        int n = nums.length;
+        int []ans = new int[n-k+1];
+        int x = 0;
+        for(int i=0;i<nums.length;i++){
+            //remove the elements that are out of range k
+            while(!deq.isEmpty() && deq.peek() == i-k){
+                //removing index from head
+                deq.poll();
+            }
+            
+            //remove the elements that are smaller in queue
+            while(!deq.isEmpty() && nums[deq.peekLast()] <= nums[i]){
+                //removing index from tail
+                deq.pollLast();
+            }
+
+            deq.offer(i);
+
+            if(i>=k-1){
+                ans[x++] = nums[deq.peek()];
+            }
+        }
+        return ans;
+    }
+}
