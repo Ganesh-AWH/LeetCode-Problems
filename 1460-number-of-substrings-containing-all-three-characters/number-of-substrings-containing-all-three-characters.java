98b14f07e0e6d0ede16c2d9899a5c51f16308568
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-03-19 20:49:44 +0530

    Time: 16 ms (43.45%) | Memory: 44.2 MB (92.78%) - LeetSync

---
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        //sliding window approach
+        int left = 0;
+        int right = 0;
+        int occurrences = 0;
+        int n = s.length();
+        //frequency array
+        int []abc = new int[3];
+        while(right < n){
+            abc[s.charAt(right) - 'a']++;
+            //these condition refers all 'abc' occurs from left to right 
+            //so  n - right also the contains these 'abc'
+            while(abc[0] > 0 && abc[1] > 0 && abc[2] > 0){
+                occurrences += n - right;
+                abc[s.charAt(left) - 'a']--;
+                left++;
+            }
+            right++;
+        }
+        return occurrences;
+    }
+}
