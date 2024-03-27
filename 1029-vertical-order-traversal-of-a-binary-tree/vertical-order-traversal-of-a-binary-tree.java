efec9e5fecec4de07aea775a5cc7570192f7af30
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-03-27 18:27:02 +0530

    Time: 3 ms (81.83%) | Memory: 42.3 MB (62.05%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -16,47 +16,21 @@
 class Solution {
     class Tuple{
         TreeNode node;
-        int hor;
         int ver;
+        int hor;
         Tuple(TreeNode node,int ver,int hor){
             this.node = node;
-            this.hor = hor;
             this.ver = ver;
+            this.hor = hor;
         }
     }
     public List<List<Integer>> verticalTraversal(TreeNode root) {
+        //inorder traversal
+        TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map = new TreeMap<>();
+        Tuple tup = new Tuple(root,0,0);
+        helper(tup,map);
+
         List<List<Integer>> ans = new ArrayList<>();
-
-        //Tree map
-        //vertical-lvl //horizontal level //elements is ascending order
-        TreeMap<Integer, TreeMap<Integer,PriorityQueue<Integer>>> map = new TreeMap<>();
-        Queue<Tuple> queue = new LinkedList<>();
-        queue.add(new Tuple(root,0,0));
-
-        //using level order traversal
-        while(!queue.isEmpty()){
-            Tuple tuple = queue.poll();
-            TreeNode node = tuple.node;
-            int v = tuple.ver;
-            int h = tuple.hor;
-
-            if(!map.containsKey(v)){
-                map.put(v,new TreeMap<>());
-            }
-            if(!map.get(v).containsKey(h)){
-                map.get(v).put(h,new PriorityQueue<>());
-            }
-            map.get(v).get(h).add(node.val);
-
-            if(node.left != null){
-                queue.add(new Tuple(node.left,v-1,h+1));
-            }
-            if(node.right != null){
-                queue.add(new Tuple(node.right,v+1,h+1));
-            }
-
-        }
-        
         for(TreeMap<Integer,PriorityQueue<Integer>> verticals : map.values()){
             List<Integer> list = new ArrayList<>();
             for(PriorityQueue<Integer> nodes : verticals.values()){
@@ -67,6 +41,27 @@ class Solution {
             ans.add(list);
         }
 
+
         return ans;
+    }
+    public void helper(Tuple tup,TreeMap<Integer,TreeMap<Integer,PriorityQueue<Integer>>> map){
+        if(tup.node == null) return;
+        
+        TreeNode node = tup.node;
+        int ver = tup.ver;
+        int hor = tup.hor;
+        if(!map.containsKey(ver)) map.put(ver,new TreeMap<>());
+        if(!map.get(ver).containsKey(hor)) map.get(ver).put(hor,new PriorityQueue<>());
+        map.get(ver).get(hor).add(node.val);
+
+        tup.node = node.left;
+        tup.ver = ver - 1;
+        tup.hor = hor + 1;
+        helper(tup,map);
+        
+        tup.node = node.right;
+        tup.ver = ver + 1;
+        tup.hor = hor + 1;        
+        helper(tup,map);
     }
 }
