f082fc8f562d7fa267ba06dd847bd147b82b3f94
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-03-31 19:13:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (96.23%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.java
@@ -15,21 +15,37 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        //iterative approach
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode temp = root;
+        //Morris inorder traversal which gives you o(n) time complexity and o(1) space complexity
+        //Morris traversal consists of threaded binary tree
         List<Integer> ans = new ArrayList<>();
-        while(true){
-            if(temp!=null){
-                stack.push(temp);
-                temp = temp.left;
-            }else{
-                if(stack.isEmpty()) break;
-                temp = stack.pop();
-                ans.add(temp.val);
-                temp = temp.right;
+        TreeNode curr = root;
+        while(curr!=null){
+            //case -1 if left is null then print root and go for right
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+            //case -2 go for left and point thread 
+            else{
+                TreeNode prev = curr.left;
+                while(prev.right!=null && prev.right != curr){
+                    prev = prev.right;
+                }
+                //instead pointing null make thread to current
+                if(prev.right == null){
+                    prev.right = curr;
+                    curr = curr.left;
+                }
+                //this indicates there already a thread
+                else{
+                    //remove the thread
+                    prev.right = null;
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
             }
         }
+
 
         return ans;
     }
