13a8af13b79c3ca29d9b04760ea1e5f30d09e212
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-03-31 19:22:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (75.18%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -15,15 +15,38 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> preorder = new ArrayList<>();
-        helper(root,preorder);
-        return preorder;
-    }
-    public void helper(TreeNode root,List<Integer> ans){
-        if(root == null) return;
+        //Morris preorder traversal which gives you o(n) time complexity and o(1) space complexity
+        //Morris traversal consists of threaded binary tree
+        List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr!=null){
+            //case -1 if left is null then print root and go for right
+            if(curr.left == null){
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+            //case -2 go for left and point thread 
+            else{
+                TreeNode prev = curr.left;
+                while(prev.right!=null && prev.right != curr){
+                    prev = prev.right;
+                }
+                //instead pointing null make thread to current
+                if(prev.right == null){
+                    prev.right = curr;
+                    ans.add(curr.val);
+                    curr = curr.left;
+                }
+                //this indicates there already a thread
+                else{
+                    //remove the thread
+                    prev.right = null;
+                    curr = curr.right;
+                }
+            }
+        }
 
-        ans.add(root.val);
-        helper(root.left,ans);
-        helper(root.right,ans);
+
+        return ans;
     }
 }
