10b987a7c0d9084b5a44f438afa51903ad51f869
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-05-12 13:25:03 +0530

    Time: 3 ms (62.99%) | Memory: 45.3 MB (75.82%) - LeetSync

---
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] largestLocal(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int [][]ans = new int[row-2][col-2];
+        for(int i=0;i<row-2;i++){
+            for(int j=0;j<col-2;j++){
+                ans[i][j] = findGreatest(grid,i,i+2,j,j+2);
+            }
+        }
+        return ans;
+    }
+    public static int findGreatest(int [][]grid,int iStart,int iEnd,int jStart,int jEnd){
+        int max = Integer.MIN_VALUE;
+        for(int i=iStart;i<=iEnd;i++){
+            for(int j=jStart;j<=jEnd;j++){
+                max = Integer.max(max,grid[i][j]);
+            }
+        }
+        return max;
+    }
+}
