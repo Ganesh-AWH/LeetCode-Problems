ad66a01db3d9557fe4e7f3b487007bfc5dbfb321
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-05-28 12:18:49 +0530

    Time: 7 ms (73.45%) | Memory: 42.7 MB (95.34%) - LeetSync

---
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int equalSubstring(String s, String t, int maxCost) {
+        //first find the difference of the ascii of the characters
+        //e.g. s=abcd , t=bcdf 
+        //difference = 1 1 1 2
+        //use sliding window approach to find the max length within maxcost 
+        //for the difference dont store it in any extra array calculate by pointing
+        int maxLength = Integer.MIN_VALUE;
+        int currentCost = 0;
+        int l=0;
+        int r=0;
+        //two pointer approach
+        for(;r<s.length();r++){
+            currentCost += Math.abs(s.charAt(r) - t.charAt(r));
+            while(currentCost > maxCost){
+                currentCost -= Math.abs(s.charAt(l) - t.charAt(l));
+                l++;
+            }
+            maxLength = Integer.max(maxLength,r-l+1);
+        }
+        return maxLength;
+    }
+}
