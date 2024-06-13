8a113b6c15b5b4b2aa7b65401b822d1b1de9b955
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-13 07:22:24 +0530

    Time: 4 ms (47.96%) | Memory: 43.9 MB (60.34%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,19 +1,21 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        //memoization - solution
+        //tabulation - solution
         int n = triangle.size();
         int [][]dp = new int[n][n];
-        for(int []row:dp){
-            Arrays.fill(row,-1);
-        }
-        return recursion(0,0,triangle,n,dp);
-    }
-    public int recursion(int i,int j,List<List<Integer>> triangle,int n,int [][]dp){
-        if(i==n-1) return triangle.get(n-1).get(j);
-        if(dp[i][j] != -1) return dp[i][j];
-        int down = triangle.get(i).get(j) + recursion(i+1,j,triangle,n,dp);
-        int diagonal = triangle.get(i).get(j) + recursion(i+1,j+1,triangle,n,dp);
 
-        return dp[i][j] = Integer.min(down,diagonal);
+        //base case
+        for(int j=0;j<n;j++){
+            dp[n-1][j] = triangle.get(n-1).get(j);
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle.get(i).get(j) + dp[i+1][j];
+                int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j] = Integer.min(down,diagonal);
+            }
+        }
+        return dp[0][0];
     }
 }
