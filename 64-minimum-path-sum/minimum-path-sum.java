e19f97054eb39a2ef5dbf7b847c61975670bd168
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-13 06:42:24 +0530

    Time: 5 ms (12.04%) | Memory: 45.5 MB (27.77%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,25 +1,27 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        //memoization - solution
+        //tabulation solution
         int m = grid.length;
         int n = grid[0].length;
         int [][]dp = new int[m][n];
-        for(int []row:dp){
-            Arrays.fill(row,-1);
-        }
-        return recursion(m-1,n-1,grid,dp);
-    }
-    public int recursion(int i,int j,int [][]grid,int [][]dp){
-        if(i==0 && j==0) return grid[0][0];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) dp[0][0] = grid[0][0];
 
-        //because we have to find minimum path sum
-        if(i<0 || j<0) return (int)(1e9);
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        int up = grid[i][j] + recursion(i-1,j,grid,dp);
-        int left = grid[i][j] + recursion(i,j-1,grid,dp);
+                else{
+                    int up = grid[i][j];
+                    int left = grid[i][j];
 
-        return dp[i][j] = Integer.min(up,left);
+                    if(i>0) up += dp[i-1][j];
+                    else up += (int)(1e9);
+                    
+                    if(j>0) left += dp[i][j-1];
+                    else left += (int)(1e9);
+                    
+                    dp[i][j] = Integer.min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 }
