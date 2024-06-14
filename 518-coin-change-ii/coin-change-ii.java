66b4dd81ec490af9673f261bd4e0d0004c4f96d1
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-14 22:33:23 +0530

    Time: 7 ms (50.46%) | Memory: 48.1 MB (17.55%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -2,24 +2,24 @@ class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
         int [][]dp = new int[n][amount+1]; 
-        for(int []row:dp){
-            Arrays.fill(row,-1);
+        //tabulation solution
+        for(int i=0;i<n;i++){
+            dp[i][0] = 1;
         }
-        return recursion(n-1,amount,coins,dp);
-    }
-    public int recursion(int index,int t,int []coins,int [][]dp){
-        if(t == 0) return 1;
-        if(index == 0){
-            if(t % coins[0] == 0) return 1;
-            else return 0;
-        }
-        if(dp[index][t] != -1) return dp[index][t];
-        int notPick = recursion(index-1,t,coins,dp);
-        int pick = 0;
-        if(coins[index]<=t){
-            pick = recursion(index,t-coins[index],coins,dp);
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0] == 0) dp[0][t] = 1;
         }
 
-        return dp[index][t] = pick + notPick;
+        for(int index = 1;index<n;index++){
+            for(int t=0;t<=amount;t++){
+                int notPick = dp[index-1][t];
+                int pick = 0;
+                if(coins[index]<=t){
+                    pick = dp[index][t-coins[index]];
+                }
+                dp[index][t] = pick + notPick;
+            }
+        }
+        return dp[n-1][amount];
     }
 }
