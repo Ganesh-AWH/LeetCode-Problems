a30da51d373124e8db836d0ca61d26139e21a4cd
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-14 22:35:53 +0530

    Time: 6 ms (60.46%) | Memory: 44.7 MB (70.54%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,25 +1,25 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int [][]dp = new int[n][amount+1]; 
-        //tabulation solution
-        for(int i=0;i<n;i++){
-            dp[i][0] = 1;
-        }
+        int []prev = new int[amount+1]; 
+        //tabulation solution with space optimization 
+        prev[0] = 1;
         for(int t=0;t<=amount;t++){
-            if(t%coins[0] == 0) dp[0][t] = 1;
+            if(t%coins[0] == 0) prev[t] = 1;
         }
 
         for(int index = 1;index<n;index++){
+            int []curr = new int[amount+1];
             for(int t=0;t<=amount;t++){
-                int notPick = dp[index-1][t];
+                int notPick = prev[t];
                 int pick = 0;
                 if(coins[index]<=t){
-                    pick = dp[index][t-coins[index]];
+                    pick = curr[t-coins[index]];
                 }
-                dp[index][t] = pick + notPick;
+                curr[t] = pick + notPick;
             }
+            prev = curr;
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 }
