3740b8ac0ed69ba74ce24c6ef31e9e54e32fc451
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-16 16:10:19 +0530

    Time: 14 ms (66.68%) | Memory: 48.2 MB (83.32%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,32 +1,29 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        //recursion solution
+        //tabulation solution solution
         int n = s.length();
         int m = t.length();
-        int [][]dp = new int[n][m];
-        for(int []row:dp){
-            Arrays.fill(row, -1);
+        int [][]dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
         }
-        return recursion(n-1, m-1, s, t, dp);
-    }
-    public int recursion(int i, int j, String s, String t, int [][]dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    //call for two possibilites
+                    //take the character from string-1
+                    int call1 = dp[i-1][j-1];
 
-        if(dp[i][j] != -1) return dp[i][j];
-        //if the both the character matches
-        if(s.charAt(i) == t.charAt(j)){
-            //call for two possibilites
-            //take the character from string-1
-            int call1 = recursion(i-1, j-1, s, t, dp);
+                    //don't take the character from string1
+                    int call2 = dp[i-1][j];
 
-            //don't take the character from string1
-            int call2 = recursion(i-1, j, s, t, dp);
-
-            return dp[i][j] = call1 + call2;
-        }else{
-            //reduce the string1 if they dont match
-            return dp[i][j] = recursion(i-1, j, s, t, dp);
+                    dp[i][j] = call1 + call2;
+                }else{
+                    //reduce the string1 if they dont match
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
+        return dp[n][m];
     }
 }
