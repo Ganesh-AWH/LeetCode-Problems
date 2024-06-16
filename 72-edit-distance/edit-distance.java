9b83b91e43e4923a0a1377d289202abe85521e44
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-16 17:23:19 +0530

    Time: 5 ms (65.90%) | Memory: 44.5 MB (87.99%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,47 +1,34 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        // memoization solution
+        // tabulation solution
         int n = word1.length();
         int m = word2.length();
         //since the base in recursion going negative shifting the index for tabulation method
         int [][]dp = new int[n+1][m+1];
-        for(int []row: dp){
-            Arrays.fill(row,-1);
+        //base case conversion
+        for(int j=0; j<=m; j++){
+            dp[0][j] = j;
         }
-        return recursion(n, m, word1, word2, dp);
-    }
-    public int recursion(int i, int j, String word1, String word2, int [][]dp){
-        //base case
-        // str1 = "" | str2 = ro
-        if(i == 0){
-            // to convert empty string to "ro" we need to insert 2 characters
-            return j;
-        }
-        //str1 = hor | str2 = ""
-        if(j == 0){
-            return i;
+        for(int i=0; i<=n; i++){
+            dp[i][0] = i;
         }
 
-        if(dp[i][j] != -1) return dp[i][j];
-        //what if both character matches
-        if(word1.charAt(i-1) == word2.charAt(j-1)){
-            //since we are not performing any operaion so 0
-            return dp[i][j] = 0 + recursion(i-1, j-1, word1, word2, dp);
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    //since we are not performing any operaion so 0
+                    dp[i][j] = 0 + dp[i-1][j-1];
+                }
+                else{
+                    int insert = 1 + dp[i][j-1];
+                    int delete = 1 + dp[i-1][j];
+                    int replace = 1 + dp[i-1][j-1];
+
+                    dp[i][j] = Integer.min(insert, Integer.min(delete, replace));
+                }
+
+            }
         }
-
-        //if the character does not matches then we gonna perform all the operations
-        
-        //insertion (if we insert character to word1 then i remains same consider hypothetically we are inserting character to word1)
-                                //since the inserted character matches so j is decreased
-        int insert = 1 + recursion(i, j-1, word1, word2, dp);
-
-        //deletion (if we delete the character from word1 then i will decrease and j remains same)
-        int delete = 1 + recursion(i-1, j, word1, word2, dp);
-
-        //replace (if we replace the character in word1 then the character matches i-1 and j-1)
-        int replace = 1 + recursion(i-1, j-1, word1, word2, dp);
-
-        return dp[i][j] = Integer.min(insert, Integer.min(delete, replace));
-
+        return dp[n][m];
     }
 }
