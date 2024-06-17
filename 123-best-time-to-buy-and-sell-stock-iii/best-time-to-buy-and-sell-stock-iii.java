ab65c645c24d9fd54f6454fcf9f989314c33405b
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-17 10:40:15 +0530

    Time: 65 ms (41.69%) | Memory: 62.4 MB (39.14%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,30 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        //dp solution
+        //tabulation solution
         int n = prices.length;
-        int [][][]dp = new int[n][2][3];
-        for(int [][]row1:dp){
-            for(int []row2: row1){
-                Arrays.fill(row2, -1);
+        int [][][]dp = new int[n+1][2][3];
+
+        for(int index=n-1; index>=0; index--){
+            for(int buy=0; buy<=1; buy++){
+                for(int tran=1; tran<=2; tran++){
+                    int profit = 0;
+                    if(buy == 1){                                       
+                        int take = -prices[index] + dp[index+1][0][tran];
+                        int notTake = 0 + dp[index+1][1][tran];
+                        profit = Integer.max(take, notTake);
+                    }else{                                        
+                        int take = prices[index] + dp[index+1][1][tran-1];
+                        int notTake = 0 + dp[index+1][0][tran];
+                        profit = Integer.max(take, notTake);
+                    }
+                    dp[index][buy][tran] = profit;
+                }
             }
         }
-        return recursion(0, 1, 2, prices, dp);
-    }
-    public int recursion(int index, int buy, int tran, int []prices, int [][][]dp){
-        //base case
-        if(index == prices.length || tran == 0) return 0;
-
-        if(dp[index][buy][tran] != -1) return dp[index][buy][tran];
-        int profit = 0;
-        if(buy == 1){                                       
-            int take = -prices[index] + recursion(index+1, 0, tran, prices, dp);
-            int notTake = 0 + recursion(index+1, 1, tran, prices, dp);
-            profit = Integer.max(take, notTake);
-        }else{                                         //since we are selling 1 transaction is completed
-            int take = prices[index] + recursion(index+1, 1, tran-1, prices, dp);
-            int notTake = 0 + recursion(index+1, 0, tran, prices, dp);
-            profit = Integer.max(take, notTake);
-        }
-        return dp[index][buy][tran] = profit;
+        return dp[0][1][2];
     }
 }
