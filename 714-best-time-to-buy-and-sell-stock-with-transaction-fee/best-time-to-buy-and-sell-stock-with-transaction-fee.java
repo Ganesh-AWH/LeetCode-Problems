2a4d278dda1e81e5ece52bbde5865eec61b26a74
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-17 11:39:42 +0530

    Time: 19 ms (45.89%) | Memory: 55.4 MB (37.18%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,29 +1,26 @@
 class Solution {
     public int maxProfit(int[] prices, int fee) {
         int n = prices.length;
-        //recursion solution
-        int [][]dp = new int[n][2];
-        for(int []row: dp) Arrays.fill(row, -1);
-        return recursion(0, 1, prices, fee, dp);
-    }
-    public int recursion(int index, int buy, int []prices, int fee, int [][]dp){
-        //base case
-        if(index == prices.length) return 0;
+        //tabulation solution
+        int [][]dp = new int[n+1][2];
 
-        if(dp[index][buy] != -1) return dp[index][buy];
+        for(int index=n-1; index>=0; index--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy == 1){
+                    int take = -prices[index] - fee + dp[index+1][0];
+                    int notTake = 0 + dp[index+1][1];
+                    profit = Integer.max(take, notTake);
+                }
+                else{
+                    int take = prices[index] + dp[index+1][1];
+                    int notTake = 0 +  dp[index+1][0];
+                    profit = Integer.max(take, notTake);
+                }
 
-        int profit = 0;
-        if(buy == 1){
-            int take = -prices[index] - fee + recursion(index+1, 0, prices, fee, dp);
-            int notTake = 0 + recursion(index+1, 1, prices, fee, dp);
-            profit = Integer.max(take, notTake);
+                dp[index][buy] = profit;
+            }
         }
-        else{
-            int take = prices[index] + recursion(index+1, 1, prices, fee, dp);
-            int notTake = 0 +  recursion(index+1, 0, prices, fee, dp);
-            profit = Integer.max(take, notTake);
-        }
-
-        return dp[index][buy] = profit;
+        return dp[0][1];
     }
 }
