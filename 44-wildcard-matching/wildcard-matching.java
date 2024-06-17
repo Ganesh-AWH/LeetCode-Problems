8d42bdb664f98fb20de70817f3b02ec431f7c18e
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-17 09:25:00 +0530

    Time: 19 ms (69.73%) | Memory: 48.3 MB (24.78%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,40 +2,43 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        int [][]dp = new int[n][m];
-        for(int []row: dp){
-            Arrays.fill(row, -1);
+        //tabulation solution
+        int [][]dp = new int[n+1][m+1];
+        //base case conversion
+        dp[0][0] = 1;
+        for(int i=1; i<=n; i++){
+            dp[i][0] = 0;
         }
-        return recursion(n-1, m-1, s, p, dp) == 1 ;
-    }
-    public int recursion(int i, int j, String s, String p, int [][]dp){
-        //base case
-        if(i < 0 && j < 0) return 1;
-
-        if(i>=0 && j<0) return 0;
-        
-        if(i < 0 && j>=0){
-            for(; j>= 0; j--){
-                if(p.charAt(j) != '*') return 0;
+        for(int j=1; j<=m; j++){
+            int flag = 1;
+            for(int jj=1; jj<=j; jj++){
+                if(p.charAt(jj-1) != '*'){
+                    flag = 0;
+                    break;
+                }
             }
-            return 1;
+            dp[0][j] = flag;
+        }   
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                char c1 = s.charAt(i-1);
+                char c2 = p.charAt(j-1);
+
+                if(c1 == c2 || c2 == '?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+
+                else if(c2 == '*'){
+                    int call1 = dp[i-1][j];
+                    int call2 = dp[i][j-1];
+
+                    dp[i][j] = call1 == 1 || call2 == 1 ? 1 : 0;
+                }
+
+                else dp[i][j] = 0;
+            }
         }
-        if(dp[i][j] != -1) return dp[i][j];
-
-        char c1 = s.charAt(i);
-        char c2 = p.charAt(j);
-
-        if(c1 == c2 || c2 == '?'){
-            return dp[i][j] = recursion(i-1, j-1, s, p, dp);
-        }
-
-        if(c2 == '*'){
-            int call1 = recursion(i-1, j, s, p, dp);
-            int call2 = recursion(i, j-1, s, p, dp);
-
-            return dp[i][j] = call1 == 1 || call2 == 1 ? 1 : 0;
-        }
-
-        return dp[i][j] = 0;
+        return dp[n][m] == 1;
     }
 }
