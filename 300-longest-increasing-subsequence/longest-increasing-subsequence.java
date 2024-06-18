9e1a14e572aaef340ff1d1560781b54564cc061d
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-18 06:54:43 +0530

    Time: 86 ms (21.04%) | Memory: 77 MB (17.79%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,26 +1,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        //Memoization solution
+        //tabulation solution
         int n = nums.length;
-                            //n+1 because of index is going negative
-        int [][]dp = new int[n][n+1];
-        for(int []row: dp) Arrays.fill(row, -1);
-        return recursion(0, -1, nums, dp);
-    }
-    public int recursion(int index, int prevIndex, int []nums, int [][]dp){
-        //base case
-        if(index == nums.length) return 0;
-        //not taking the element
-        if(dp[index][prevIndex+1] != -1) return dp[index][prevIndex+1];
-        int notPick = 0 + recursion(index+1, prevIndex, nums, dp);
-        //taking the element
-                            //since subsequence should be increasing
-        int pick = Integer.MIN_VALUE;
-        if(prevIndex == -1 || nums[prevIndex] < nums[index]){
-                                            //marking the index as previous
-            pick = 1 + recursion(index+1, index, nums, dp);
-        }
+        int [][]dp = new int[n+1][n+1];
 
-        return dp[index][prevIndex+1] = Integer.max(pick, notPick);
+        for(int index=n-1; index>=0; index--){
+            for(int prevIndex=index-1; prevIndex>=-1; prevIndex--){
+                int notPick = 0 + dp[index+1][prevIndex+1];
+                int pick = Integer.MIN_VALUE;
+                if(prevIndex == -1 || nums[prevIndex] < nums[index]){
+                    pick = 1 + dp[index+1][index+1];
+                }
+
+                dp[index][prevIndex+1] = Integer.max(pick, notPick);
+            }
+        }
+        return dp[0][0];
     }
 }
