4a4972f823a37856d1e706bb55f0974088f24413
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-19 10:02:11 +0530

    Time: 915 ms (50.81%) | Memory: 41.8 MB (60.93%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,24 +1,20 @@
 class Solution {
     public int minCut(String s) {
         int n = s.length();
-        //recursive approach
-        int []dp = new int[n];
-        Arrays.fill(dp, -1);
-        return recursion(0, s.toCharArray(), n, dp)-1;
-    }
-    public int recursion(int i, char []str, int n, int []dp){
-        if(i == n) return 0; 
-        if(dp[i] != -1) return dp[i];
-        //partitioning
-        int mini = Integer.MAX_VALUE;
-        for(int j=i; j<n; j++){
-            if(isPalindrome(i, j, str)){
-                int cost = 1 + recursion(j+1, str, n, dp);
-                mini = Integer.min(mini, cost);
+        //tabulation approach
+        int []dp = new int[n+1];
+        char []str = s.toCharArray();
+        for(int i=n-1; i>=0; i--){
+            int mini = Integer.MAX_VALUE;
+            for(int j=i; j<n; j++){
+                if(isPalindrome(i, j, str)){
+                    int cost = 1 + dp[j+1];
+                    mini = Integer.min(mini, cost);
+                }
             }
+            dp[i] = mini;
         }
-
-        return dp[i] = mini;
+        return dp[0] - 1;
     }
     public boolean isPalindrome(int s, int e, char []str){
         while(s<e){
