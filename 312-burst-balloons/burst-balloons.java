462077f883ee70eecd5228ed90fe5041eeebbf7f
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-06-19 08:39:28 +0530

    Time: 233 ms (5.23%) | Memory: 43.1 MB (10.03%) - LeetSync

---
+++ 312-burst-balloons/burst-balloons.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        //memoization approach
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        for(int i: nums) list.add(i);
+        list.add(1);
+        int n = list.size();
+        int  [][]dp = new int[n+1][n+1];
+        for(int []row: dp) Arrays.fill(row, -1);
+        return recursion(1, n-2, list, dp);
+    }
+    public int recursion(int i, int j, List<Integer> list, int [][]dp){
+        if(i > j) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+        int maxi = Integer.MIN_VALUE;
+        for(int index = i; index<=j; index++){
+            int coins = list.get(i-1) * list.get(index) * list.get(j+1) + 
+            recursion(i, index-1, list, dp) + recursion(index+1, j, list, dp);
+
+            maxi = Integer.max(coins, maxi);
+        }
+        return dp[i][j] = maxi;
+    }
+}
