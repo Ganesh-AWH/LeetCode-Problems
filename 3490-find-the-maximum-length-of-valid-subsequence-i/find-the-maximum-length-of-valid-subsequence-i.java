e20f2be6e1245145e3b73a39cd79687d88e89f76
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-07-02 15:25:44 +0530

    Time: 12 ms (51.04%) | Memory: 61.8 MB (94.15%) - LeetSync

---
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maximumLength(int[] nums) {
+        //case 1 and case 2 every even and every odd
+        int odd = 0;
+        int even = 0;
+        for(int i: nums){
+            if(i % 2 == 0){
+                even++;
+            }else{
+                odd++;
+            }
+        }
+        //max of case1 and case2
+        int max = Integer.max(odd, even);
+        //for case 3 sub0 = odd + sub1 = even + sub2 = odd;
+        //false = odd and true = even
+        max = Integer.max(max, helper(nums, false)); 
+        max = Integer.max(max, helper(nums, true));
+
+        return max;
+    }
+    public int helper(int []nums, boolean flag){
+        int len = 0;
+        for(int i=0; i<nums.length; i++){
+            //go for odd
+            if(flag && nums[i]%2 == 1){
+                len++;
+                flag = false;
+            }else if(!flag && nums[i]%2 == 0){
+                len++;
+                flag = true;
+            }
+        }
+
+        return len;
+    }
+}
