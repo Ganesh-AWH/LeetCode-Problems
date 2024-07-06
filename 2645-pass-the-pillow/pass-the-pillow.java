ee624c898d6f2f5a41a635f26ae21ff8fa4d90b0
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-07-06 15:44:07 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (74.29%) - LeetSync

---
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        //true means forward and false mean backward
+        boolean direction = true;
+        int index = 1;
+        while(time-->0){
+            if(direction){
+                index++;
+                if(index == n){
+                    direction = false;
+                }
+            }else{
+                index--;
+                if(index == 1){
+                    direction = true;
+                }
+            }
+        }
+
+        return index;
+    }
+}
