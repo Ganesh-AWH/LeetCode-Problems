1c7fee0269aa130975be867a874cd39ed40f560b
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2024-07-24 20:31:43 +0530

    Time: 280 ms (41.18%) | Memory: 56.4 MB (13.73%) - LeetSync

---
+++ 1333-sort-the-jumbled-numbers/sort-the-jumbled-numbers.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] sortJumbled(int[] mapping, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Integer []ans = new Integer[nums.length];
+        for(int i=0; i<nums.length; i++){
+            ans[i] = nums[i];
+            int x = convert(String.valueOf(nums[i]), mapping);
+            map.put(nums[i], x);
+        } 
+        Arrays.sort(ans, (a, b) -> map.get(a) - map.get(b));
+        int []ans1 = new int[nums.length];
+        for(int i=0; i<nums.length; i++){
+            ans1[i] = ans[i];
+        }
+        return ans1;
+    }
+    public static int convert(String num, int []mapping){
+        int ans = 0;
+        for(int i=0; i<num.length(); i++){
+            int d = num.charAt(i) - '0';
+            ans = ans * 10 + mapping[d];
+        }
+        return ans;
+    }
+}
