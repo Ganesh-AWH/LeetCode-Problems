dd7c09865630b7cda04cbfccccf269ae70baf0c7
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-08-05 07:28:27 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (74.31%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -4,8 +4,15 @@ class Solution {
         int high = nums.length-1;
         int ans = Integer.MAX_VALUE;
         while(low <= high){
+            //further optimization - if the array is fully sorted then don't need of binary search
+            if(nums[low] <= nums[high]){
+                ans = Integer.min(ans, nums[low]);
+                return ans;
+            }
+
             //first find which part is sorted either left or right
             int mid = (high-low)/2+low;
+
             if(nums[low]<=nums[mid]){ //left portion sorted
                 //then minimum  value will be stored in leftmost part
                 ans = Integer.min(ans, nums[low]);
