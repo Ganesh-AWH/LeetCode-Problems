686bc8d90d25f021583b45599f96b31999ec5fc4
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:11:36 +0530

    Create 985-BagOfTokens

---
+++ 985-BagOfTokens/985-BagOfTokens.java
@@ -0,0 +1,27 @@
+// Last updated: 9/14/2025, 9:11:33 AM
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int i = 0;
+        int j = tokens.length-1;
+        int score = 0;
+        int maxScore = 0;
+        while(i<=j){
+            if(tokens[i]<=power){
+                score++;
+                power = power - tokens[i];
+                maxScore = Integer.max(score,maxScore);
+                i++;
+            }
+            else if(score >= 1){
+                power += tokens[j];
+                score--;
+                j--;
+            }else{
+                break;
+            }
+        }
+
+        return maxScore;
+    }
+}
