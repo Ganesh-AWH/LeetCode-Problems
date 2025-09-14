14dd8155dd365d3802fb8a44223e903f45c22384
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:11:57 +0530

    Create 917-BoatsToSavePeople

---
+++ 917-BoatsToSavePeople/917-BoatsToSavePeople.java
@@ -0,0 +1,22 @@
+// Last updated: 9/14/2025, 9:11:54 AM
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        //two pointers approach
+        int i=0;
+        int j=people.length-1;
+        int count = 0;
+        while(i<=j){
+            if(people[i]+people[j] <= limit){
+                i++;
+                j--;
+            }
+            else{
+                j--;
+            }
+            count++;
+        }
+
+        return count;
+    }
+}
