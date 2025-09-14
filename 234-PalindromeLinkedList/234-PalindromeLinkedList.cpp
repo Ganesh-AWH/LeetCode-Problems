c1c0e6be88efc225c28c6c4928dd4a6cb998e408
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:15:25 +0530

    Create 234-PalindromeLinkedList

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.cpp
@@ -0,0 +1,31 @@
+// Last updated: 9/14/2025, 9:15:22 AM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int>s;
+        ListNode *temp=head;
+        while(temp){
+            s.push(temp->val);
+            temp = temp->next;
+        }
+
+        while(head){
+            if(head->val!=s.top()){
+                return false;
+            }
+            s.pop();
+            head=head->next;
+        }
+        return true;
+    }
+};
