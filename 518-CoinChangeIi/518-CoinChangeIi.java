4dc1189d12271107675feff65ba3768e4392ca46
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:13:41 +0530

    Create 518-CoinChangeIi

---
+++ 518-CoinChangeIi/518-CoinChangeIi.java
@@ -0,0 +1,26 @@
+// Last updated: 9/14/2025, 9:13:39 AM
+class Solution {
+    public int change(int amount, int[] coins) {
+        int n = coins.length;
+        int []prev = new int[amount+1]; 
+        //tabulation solution with space optimization 
+        prev[0] = 1;
+        for(int t=0;t<=amount;t++){
+            if(t%coins[0] == 0) prev[t] = 1;
+        }
+
+        for(int index = 1;index<n;index++){
+            int []curr = new int[amount+1];
+            for(int t=0;t<=amount;t++){
+                int notPick = prev[t];
+                int pick = 0;
+                if(coins[index]<=t){
+                    pick = curr[t-coins[index]];
+                }
+                curr[t] = pick + notPick;
+            }
+            prev = curr;
+        }
+        return prev[amount];
+    }
+}
