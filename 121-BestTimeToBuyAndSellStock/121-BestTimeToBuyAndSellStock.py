dc2dc80b73cee6008fa79a8bf75826afe490deb8
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:17:01 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.py
@@ -0,0 +1,12 @@
+# Last updated: 9/14/2025, 9:16:59 AM
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for i in range(len(prices)):
+            if prices[i] < min_price:
+                min_price = prices[i]
+            elif prices[i] - min_price > max_profit:
+                max_profit = prices[i] - min_price
+                
+        return max_profit
