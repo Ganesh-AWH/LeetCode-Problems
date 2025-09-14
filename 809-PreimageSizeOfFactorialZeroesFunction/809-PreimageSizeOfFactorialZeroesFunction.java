34dd4e6fc7153aa66edf687b32bd12b525a5e3ca
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:12:22 +0530

    Create 809-PreimageSizeOfFactorialZeroesFunction

---
+++ 809-PreimageSizeOfFactorialZeroesFunction/809-PreimageSizeOfFactorialZeroesFunction.java
@@ -0,0 +1,23 @@
+// Last updated: 9/14/2025, 9:12:20 AM
+class Solution {
+    public int preimageSizeFZF(int k) {
+        long n = 4L * k;
+        int resp = 0;
+        while (true) {
+            int t = zeros(n);
+            if (t > k) return 0;
+            if (t == k) return 5;
+            n++;
+        }
+    
+    }
+    
+    private int zeros(long n) {
+        int resp = 0;
+        while (n > 0) {
+            resp += (int)(n / 5);
+            n /= 5;
+        }
+        return resp;
+    }
+}
