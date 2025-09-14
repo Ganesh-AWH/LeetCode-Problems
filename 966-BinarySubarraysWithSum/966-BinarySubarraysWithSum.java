0b29d5e5c2e07a569c848ba54f683bd3bdc6d6e8
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:11:47 +0530

    Create 966-BinarySubarraysWithSum

---
+++ 966-BinarySubarraysWithSum/966-BinarySubarraysWithSum.java
@@ -0,0 +1,19 @@
+// Last updated: 9/14/2025, 9:11:44 AM
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        //solving using prefix sum approach
+        int total = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        int sum = 0;
+        map.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            sum += nums[i];
+            int x = sum - goal;
+            if(map.containsKey(x)){
+                total += map.get(x);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return total;
+    }
+}
