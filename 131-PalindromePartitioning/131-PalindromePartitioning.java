1360208b092c28d5e743238056c40f37d7ae5afb
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:16:45 +0530

    Create 131-PalindromePartitioning

---
+++ 131-PalindromePartitioning/131-PalindromePartitioning.java
@@ -0,0 +1,28 @@
+// Last updated: 9/14/2025, 9:16:43 AM
+class Solution {
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        helper(s,0,ans,new ArrayList<>());
+        return ans;
+    }
+    private static void helper(String s,int index,List<List<String>> ans,List<String> temp){
+        if(index == s.length()){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i=index;i<s.length();i++){
+            if(isPalindrome(s,index,i)){
+                temp.add(s.substring(index,i+1));
+                helper(s,i+1,ans,temp);
+                temp.remove(temp.size()-1);
+            }
+        }
+    }
+    private static boolean isPalindrome(String str,int s,int e){
+        while(s<=e){
+            if(str.charAt(s++) != str.charAt(e--)) return false;
+        }
+        return true;
+    }
+}
