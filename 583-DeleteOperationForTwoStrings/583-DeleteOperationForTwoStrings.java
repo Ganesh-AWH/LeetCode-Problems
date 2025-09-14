0a0c49fff07ae41fdc1f26ec83dd214b67732b76
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:13:19 +0530

    Create 583-DeleteOperationForTwoStrings

---
+++ 583-DeleteOperationForTwoStrings/583-DeleteOperationForTwoStrings.java
@@ -0,0 +1,32 @@
+// Last updated: 9/14/2025, 9:13:17 AM
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int len = longestCommonSubsequence(word1,word2);
+        return (n - len) + (m - len) ;
+    }
+    public int longestCommonSubsequence(String text1, String text2) {
+        //tabulation solution with space optimization
+        //shifting the index -1 will become 0, 0->1 ....
+        int l1 = text1.length();
+        int l2 = text2.length();
+        int []prev = new int[l2+1];
+
+        for(int index1=1; index1<=l1; index1++){
+            int []curr = new int[l2+1];
+            for(int index2=1; index2<=l2; index2++){
+                if(text1.charAt(index1-1) == text2.charAt(index2-1))
+                    curr[index2] = 1 + prev[index2-1];
+                else{
+                    //if the character does not matches
+                    int part1 = 0 + prev[index2];
+                    int part2 = 0 + curr[index2-1];
+                    curr[index2] = Integer.max(part1,part2);
+                }
+            }
+            prev = curr;
+        }
+        return prev[l2];
+    }
+}
