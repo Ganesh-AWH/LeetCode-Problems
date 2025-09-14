fc31b767f116ace18e2ef8b096c0980ae147e69e
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:09:49 +0530

    Create 1669-MinimumCostToCutAStick

---
+++ 1669-MinimumCostToCutAStick/1669-MinimumCostToCutAStick.java
@@ -0,0 +1,28 @@
+// Last updated: 9/14/2025, 9:09:46 AM
+class Solution {
+    public int minCost(int n, int[] cuts) {
+        // tabulation approach
+        List<Integer> list = new ArrayList<>();
+        list.add(0);
+        for(int i: cuts) list.add(i);
+        list.add(n);
+        Collections.sort(list);
+        
+        int len = cuts.length;
+        int [][]dp = new int[len+2][len+2];
+
+        int m = list.size();
+        for(int i=m-2; i>=1; i--){
+            for(int j=1; j<=m-2; j++){
+                if(i > j) continue;
+                int mini = Integer.MAX_VALUE;
+                for(int k=i; k<=j; k++){
+                    int cost = list.get(j+1) - list.get(i-1) + dp[i][k-1] + dp[k+1][j];
+                    mini = Integer.min(mini, cost); 
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][m-2];
+    }
+}
