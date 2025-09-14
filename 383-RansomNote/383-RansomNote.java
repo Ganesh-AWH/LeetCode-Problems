fbae48c49c3e0d5143ef0735956462c09002abde
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:14:27 +0530

    Create 383-RansomNote

---
+++ 383-RansomNote/383-RansomNote.java
@@ -0,0 +1,22 @@
+// Last updated: 9/14/2025, 9:14:25 AM
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character,Integer> map = new HashMap<>();
+
+        for(char c:magazine.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        for(char c:ransomNote.toCharArray()){
+            if(!map.containsKey(c) || map.get(c)<=0){
+                return false;
+            }else{
+                map.put(c,map.get(c)-1);
+            }
+        }
+
+        return true;
+    }
+}
+
+//use array to reduce the time complexity 
