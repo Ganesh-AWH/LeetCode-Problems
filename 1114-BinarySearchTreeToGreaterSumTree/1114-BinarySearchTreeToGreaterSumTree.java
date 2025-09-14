2b06d25456264281362473ef6785b67ef4bdee5a
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:11:15 +0530

    Create 1114-BinarySearchTreeToGreaterSumTree

---
+++ 1114-BinarySearchTreeToGreaterSumTree/1114-BinarySearchTreeToGreaterSumTree.java
@@ -0,0 +1,29 @@
+// Last updated: 9/14/2025, 9:11:12 AM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int sum = 0;
+    
+    public TreeNode bstToGst(TreeNode root) {  
+        if (root != null) {
+            bstToGst(root.right);  // Traverse the right subtree
+            sum += root.val;  // Update the sum
+            root.val = sum;  // Update the current node's value
+            bstToGst(root.left);  // Traverse the left subtree
+        }
+        return root;
+    }
+}
