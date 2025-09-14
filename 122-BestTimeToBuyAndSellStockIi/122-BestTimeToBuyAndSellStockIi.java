8511ff3eef81d8432d6f2257abe9a50fc8fe8239
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:16:55 +0530

    Create 122-BestTimeToBuyAndSellStockIi

---
+++ 122-BestTimeToBuyAndSellStockIi/122-BestTimeToBuyAndSellStockIi.java
@@ -0,0 +1,28 @@
+// Last updated: 9/14/2025, 9:16:53 AM
+class Solution {
+    public int maxProfit(int[] prices) {
+        //tabulation solution with space optimization
+        int n = prices.length;
+        int []front = new int[2];
+        front[0] = front[1] = 0;
+        for(int index=n-1; index>=0; index--){
+            int []curr = new int[2];
+            for(int buy = 1; buy>=0; buy--){
+                int profit = 0;
+                if(buy == 1){
+                    int take = -prices[index] + front[0];
+                    int notTake = 0 + front[1];
+                    profit = Integer.max(take, notTake);
+                }else{
+                    int take = +prices[index] + front[1];
+                    int notTake = 0 + front[0];
+                    profit = Integer.max(take, notTake);
+                }
+
+                curr[buy] = profit;
+            }
+            front = curr;
+        }
+        return front[1];
+    }
+}
