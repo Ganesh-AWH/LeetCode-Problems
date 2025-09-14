fb9f9fdc59cb142763f25116af4a8001ae5639e4
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:09:38 +0530

    Create 1730-SpecialArrayWithXElementsGreaterThanOrEqualX

---
+++ 1730-SpecialArrayWithXElementsGreaterThanOrEqualX/1730-SpecialArrayWithXElementsGreaterThanOrEqualX.java
@@ -0,0 +1,26 @@
+// Last updated: 9/14/2025, 9:09:36 AM
+class Solution {
+    public int specialArray(int[] nums) {
+           int start = 0;
+           int end = nums.length;
+           int mid = 0;
+           while(start <= end){
+               mid = start + (end - start) /2;
+               int count = 0;
+               for(int i=0 ; i<nums.length ; i++){
+                   if(nums[i]>= mid){
+                       count++;
+                   }
+               }
+               if(count == mid) {
+                   return count;
+               }
+               else if(count > mid){
+                   start = mid + 1;
+               }else{
+                   end = mid - 1;
+               }
+           }
+           return -1;
+    }
+}
