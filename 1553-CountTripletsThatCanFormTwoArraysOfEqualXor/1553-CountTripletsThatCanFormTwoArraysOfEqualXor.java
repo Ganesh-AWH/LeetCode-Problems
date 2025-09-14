2b450c5526eac3a851185d47f3c53ce9a54212c5
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:10:02 +0530

    Create 1553-CountTripletsThatCanFormTwoArraysOfEqualXor

---
+++ 1553-CountTripletsThatCanFormTwoArraysOfEqualXor/1553-CountTripletsThatCanFormTwoArraysOfEqualXor.java
@@ -0,0 +1,24 @@
+// Last updated: 9/14/2025, 9:10:00 AM
+class Solution {
+    public int countTriplets(int[] arr) {
+        int n = arr.length+1;
+        int []prefix_xor = new int[n];
+        prefix_xor[0] = 0;
+
+        for(int i=1;i<n;i++){
+            prefix_xor[i] = prefix_xor[i-1] ^ arr[i-1];
+        }
+
+        int res = 0;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(prefix_xor[i] == prefix_xor[j]){
+                    res += j - i - 1;
+                }
+            }
+        }
+
+        return res;
+    }
+}
