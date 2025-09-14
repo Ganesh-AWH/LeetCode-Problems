a71e0be652f353de1baa438d1c8cd2438d8ff6de
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:09:28 +0530

    Create 1801-AverageTimeOfProcessPerMachine

---
+++ 1801-AverageTimeOfProcessPerMachine/1801-AverageTimeOfProcessPerMachine.sql
@@ -0,0 +1,15 @@
+-- Last updated: 9/14/2025, 9:09:26 AM
+# Write your MySQL query statement below
+
+-- cross join
+
+-- b is end and a is start 
+SELECT a.machine_id, 
+       ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+FROM activity a, activity b
+WHERE 
+    a.machine_id = b.machine_id AND
+    a.process_id = b.process_id AND
+    a.activity_type = 'start' AND
+    b.activity_type = 'end'
+GROUP BY machine_id;
