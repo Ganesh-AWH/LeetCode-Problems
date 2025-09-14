e85114b11378653dcad2458c495f1834c2d8c8b8
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:09:41 +0530

    Create 1720-CrawlerLogFolder

---
+++ 1720-CrawlerLogFolder/1720-CrawlerLogFolder.java
@@ -0,0 +1,23 @@
+// Last updated: 9/14/2025, 9:09:39 AM
+class Solution {
+
+    public int minOperations(String[] logs) {
+        Stack<String> folderStack = new Stack<>();
+
+        for (String currentOperation : logs) {
+            if (currentOperation.equals("../")) {
+                // Move up to parent directory if not already at main folder
+                if (!folderStack.empty()) {
+                    folderStack.pop();
+                }
+            } else if (!currentOperation.equals("./")) {
+                // Enter a new folder
+                folderStack.push(currentOperation);
+            }
+            // Ignore "./" operations as they don't change the current folder
+        }
+
+        // The stack size represents the number of folders deep we are
+        return folderStack.size();
+    }
+}
