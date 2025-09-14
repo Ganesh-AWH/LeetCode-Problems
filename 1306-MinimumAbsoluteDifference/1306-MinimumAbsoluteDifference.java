815781397058a42603f05c01c0431f4808d382cd
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:10:44 +0530

    Create 1306-MinimumAbsoluteDifference

---
+++ 1306-MinimumAbsoluteDifference/1306-MinimumAbsoluteDifference.java
@@ -0,0 +1,22 @@
+// Last updated: 9/14/2025, 9:10:42 AM
+class Solution {
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        Arrays.sort(arr);
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<arr.length-1;i++){
+            if(Math.abs(arr[i]-arr[i+1]) < min){
+                min = Math.abs(arr[i] - arr[i+1]);
+            }
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i=0;i<arr.length-1;i++){
+            if(Math.abs(arr[i] - arr[i+1]) == min){
+                ans.add(Arrays.asList(arr[i],arr[i+1]));
+            }
+        }
+
+        return ans;
+    }
+}
