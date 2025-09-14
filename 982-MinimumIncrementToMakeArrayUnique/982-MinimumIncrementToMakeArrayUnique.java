32fb3d75a3089e193c4f7777775644c582db469f
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-14 09:11:43 +0530

    Create 982-MinimumIncrementToMakeArrayUnique

---
+++ 982-MinimumIncrementToMakeArrayUnique/982-MinimumIncrementToMakeArrayUnique.java
@@ -0,0 +1,14 @@
+// Last updated: 9/14/2025, 9:11:41 AM
+class Solution {
+    public int minIncrementForUnique(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] <= nums[i - 1]) {
+                ans += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i - 1] + 1;
+            }
+        }
+        return ans;
+    }
+}
