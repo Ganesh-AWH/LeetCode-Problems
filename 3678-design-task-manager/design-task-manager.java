55cfef10700b5ced12c238fb4d5490d4fbfd7b81
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-18 20:08:58 +0530

    Time: 365 ms (23.67%) | Memory: 173.3 MB (34.91%) - LeetSync

--- 3678-design-task-manager/design-task-manager.java
+++ 3678-design-task-manager/design-task-manager.java
@@ -1,48 +1,52 @@
-import java.util.*;
 class TaskManager {
-    Map<Integer, int[]> map; //taskId -> {userId, priority}
-    // PriorityQueue<int []> pq -> i can also use this but to remove it takes o(n)
-    //so i'm going with treeset
-    TreeSet<int []> st; //{priority, taskId}
+    Map<Integer, int []> map; //taskId -> {userId, priority}
+    TreeSet<int []> tSet; //{priority, taskId}
     public TaskManager(List<List<Integer>> tasks) {
         map = new HashMap<>();
-        st =  new TreeSet<>((a, b) ->{
-            //sort by priority
+        tSet = new TreeSet<>((a,b) ->{
             if(a[0] != b[0]) return a[0] - b[0];
-            //tie -> smaller taskId
-            return a[1] - b[1];
+            
+            else return a[1] - b[1];
         });
 
         for(List<Integer> task: tasks){
-            this.add(task.get(0), task.get(1), task.get(2));
+            int userId = task.get(0);
+            int taskId = task.get(1);
+            int priority = task.get(2);
+            this.add(userId, taskId, priority);
         }
     }
     
     public void add(int userId, int taskId, int priority) {
-        st.add(new int[]{priority, taskId});
         map.put(taskId, new int[]{userId, priority});
+        tSet.add(new int[]{priority, taskId});
     }
     
     public void edit(int taskId, int newPriority) {
-        int []entry = map.get(taskId);
-        st.remove(new int[]{entry[1], taskId}); //old {priority, taskId}
-        st.add(new int[]{newPriority, taskId});
-        map.put(taskId, new int[]{entry[0], newPriority});
+        int []entry = map.get(taskId); 
+        int userId = entry[0];
+        int priority = entry[1];
+        tSet.remove(new int[]{priority, taskId});
+        tSet.add(new int[]{newPriority, taskId});
+        map.put(taskId, new int[]{userId, newPriority}); 
     }
     
     public void rmv(int taskId) {
-        int []entry = map.get(taskId);
-        st.remove(new int[]{entry[1], taskId});
+        int []entry = map.get(taskId); 
+        int userId = entry[0];
+        int priority = entry[1];
+        tSet.remove(new int[]{priority, taskId});
         map.remove(taskId);
     }
-    
+
     public int execTop() {
         if(map.isEmpty()) return -1;
-        int []top = st.last(); //highest priority
+
+        int []top = tSet.last(); // taking the highest priority 
+        int priority = top[0];
         int taskId = top[1];
         int userId = map.get(taskId)[0];
-        st.remove(top);
-        map.remove(taskId);
+        this.rmv(taskId);
         return userId;
     }
 }
