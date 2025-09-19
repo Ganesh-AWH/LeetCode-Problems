9890fd13cba1dc6274ffac5124107ec2a6754792
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-19 22:00:01 +0530

    Time: 111 ms (84.86%) | Memory: 56.6 MB (19.37%) - LeetSync

--- 3797-design-spreadsheet/design-spreadsheet.java
+++ 3797-design-spreadsheet/design-spreadsheet.java
@@ -1,15 +1,15 @@
 class Spreadsheet {
-
     int [][]sheet;
+
     public Spreadsheet(int rows) {
-        sheet= new int[rows][26];
+        sheet = new int[rows][26];
     }
     
     public void setCell(String cell, int value) {
-        int []data = extract(cell);
-        int row = data[0];
-        int column = data[1]; 
-        this.sheet[row][column] = value;
+        int column = cell.charAt(0) - 'A';
+        int row = Integer.parseInt(cell.substring(1))-1;
+
+        sheet[row][column] = value;
     }
     
     public void resetCell(String cell) {
@@ -17,32 +17,20 @@ class Spreadsheet {
     }
     
     public int getValue(String formula) {
-        int indx = formula.indexOf('+');
-        //why 1? neglecting = symbol
-        // =A1+B2
-        //eg. left -> A11
-        String left = formula.substring(1, indx);
-        //eg right -> B278
-        String right = formula.substring(indx+1);
+        
+        int index = formula.indexOf('+');
+        String left = formula.substring(1, index);
+        String right = formula.substring(index+1);
 
-        int valLeft = Character.isLetter(left.charAt(0))
-                    ? sheet[Integer.parseInt(left.substring(1)) - 1][left.charAt(0) - 'A']
-                    :Integer.parseInt(left);
+        int leftVal = Character.isLetter(left.charAt(0)) 
+                        ? sheet[Integer.parseInt(left.substring(1)) - 1][left.charAt(0) - 'A']
+                        : Integer.parseInt(left);
 
-        int valRight = Character.isLetter(right.charAt(0)) 
-                        ? sheet[Integer.parseInt(right.substring(1)) - 1][right.charAt(0)-'A']
-                        : Integer.parseInt(right); 
+        int rightVal = Character.isLetter(right.charAt(0))
+                        ? sheet[Integer.parseInt(right.substring(1)) - 1][right.charAt(0) - 'A']
+                        : Integer.parseInt(right);
 
-        return valLeft + valRight;
-
-    }
-    //this function extracts row and columns
-    public int[] extract(String str){
-        //fetching the column number
-        int column = str.charAt(0) - 'A';
-        //fetching row number
-        int row = Integer.parseInt(str.substring(1)) - 1;
-        return new int[]{row, column};
+        return leftVal + rightVal;
     }
 }
 
@@ -53,4 +41,3 @@ class Spreadsheet {
  * obj.resetCell(cell);
  * int param_3 = obj.getValue(formula);
  */
-        
