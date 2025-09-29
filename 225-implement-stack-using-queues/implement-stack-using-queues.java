f6235db7be5a7d39b6a1d1384ad7e4fbbd90677f
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-09-29 11:54:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (97.25%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,21 +1,15 @@
 class MyStack {
-    //stack using 2 queues in coding ninja verify that also
-
-    //stack using single queue
-    //frist push the element into the queue
-    //if size > 1 remove top element and again push into the queue
     Queue<Integer> queue;
-    int size;
+
     public MyStack() {
         queue = new LinkedList<>();
-        size = 0;
     }
     
     public void push(int x) {
         queue.add(x);
-        for(int i=0;i<queue.size()-1;i++){
-            int ele = queue.remove();
-            queue.add(ele);
+        for(int i=0; i<queue.size()-1; i++){
+            int val = queue.remove();
+            queue.add(val);
         }
     }
     
