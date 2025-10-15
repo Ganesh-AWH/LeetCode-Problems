22b6f28933d457368dee2bd9e7ad2da40b6f4d71
Ganesh <98997697+Ganesh-AWH@users.noreply.github.com>
2025-10-15 14:21:40 +0530

    Time: 3 ms (70.60%) | Memory: 43.5 MB (36.49%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,27 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
+        List<List<Integer>> ans = new ArrayList<>();
         findCombination(0, candidates, target, new ArrayList<>(), ans);
         return ans;
     }
-    public static void findCombination(int i, int []arr, int target, List<Integer> temp, List<List<Integer>> ans){
+    public static void findCombination(int index, int []arr, int target, List<Integer> temp, List<List<Integer>> ans){
         if(target == 0){
             ans.add(new ArrayList<>(temp));
             return;
         }
-        if(i>=arr.length){
-            return;
-        }
 
-        if(arr[i]<=target){
-            temp.add(arr[i]);
-            findCombination(i+1, arr, target-arr[i], temp, ans);
-            temp.remove(temp.size()-1);
+        for(int i=index; i<arr.length; i++){
+            if(i!=index && arr[i] == arr[i-1]) continue;
+            if(target>=arr[i]){
+                temp.add(arr[i]);
+                findCombination(i+1, arr, target-arr[i], temp, ans);
+                temp.remove(temp.size()-1);
+            }
         }
-
-        int next = i+1;
-        while(next<arr.length && arr[next] == arr[i]) next++;
-        findCombination(next, arr, target, temp, ans);
     }
 }
